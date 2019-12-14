c2b72c71de510fa685dbf8a7a48a2a34d37e0558
Jared <jared.l.j.carey@gmail.com>
2019-12-14 17:05:03 +1000

    Created LevelGenerator Class and Basic Level Gen Functionality

---
+++ MK2019_ApplicationGame_JaredCarey/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Determines the maximum number of Terrain Sets that will be active in the scene at any given time.")]
+    [Range(0, 100)]
+    private int maxNumberOfTerrainSets;
+    [SerializeField]
+    GameObject[] terrainSets;
+    [SerializeField]
+    GameObject startingTerrainSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel(true);
+    }
+
+    public void GenerateLevel(bool spawnStartSet) 
+    {   
+        Vector3 terrainEndPoint = Vector3.zero;
+        GameObject terrainSet;
+
+        for (int i = 0; i < maxNumberOfTerrainSets; i++) 
+        {
+            if (spawnStartSet) 
+            {
+                terrainSet = Instantiate(startingTerrainSet);
+                terrainEndPoint = terrainSet.transform.GetChild(1).position;
+            }
+            else 
+            {
+                terrainSet = Instantiate(terrainSets[0], terrainEndPoint, Quaternion.identity);
+                terrainEndPoint = terrainSet.transform.GetChild(1).position;
+            }
+        }
+    }
+}
