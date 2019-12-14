1c7ef6a02f321490c4235c323ab3a24bcfc37f91
Jared <jared.l.j.carey@gmail.com>
2019-12-14 18:20:02 +1000

    Corrected Syntax, Setup Jump Functionality

--- MK2019_ApplicationGame_JaredCarey/Assets/Scripts/Player.cs
+++ MK2019_ApplicationGame_JaredCarey/Assets/Scripts/Player.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     [Tooltip("The player's default movement speed")]
-    [Range(0, 5)]
+    [Range(0, 10)]
     public float movementSpeed = 1;
+
+    [Tooltip("How high the player will jump")]
+    [Range(0, 10)]
+    public float jumpForce = 1;
+
+    Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,14 +30,15 @@ public class Player : MonoBehaviour
     {
         #if UNITY_EDITOR
 
-            if(Input.getKeyDown(Keycode.Space) 
-            || Input.getKeyDown(Keycode.UpArrow)
-            || Input.getKeyDown(Keycode.W))
+            if(Input.GetKeyDown(KeyCode.Space)
+            || Input.GetKeyDown(KeyCode.UpArrow)
+            || Input.GetKeyDown(KeyCode.W))
             {
-                // TODO: Jump
+                Debug.Log("PC Input Detected");
+                Jump();
             }
 
-            if(Input.getKeyDown(Keycode.Escape))
+            if(Input.GetKeyDown(KeyCode.Escape))
             {
                 // TODO: Pause the Game
             }
@@ -34,15 +47,27 @@ public class Player : MonoBehaviour
 
             // TODO: Mobile Input
 
-        #endif 
+        #endif
     }
 
     void Movement()
     {
         float translate = movementSpeed * Time.deltaTime;
-        Vector3 playerPos = transform.position;
+        Vector2 playerPos = transform.position;
 
-        playerPos = new Vector3(playerPos.x + translate, playerPos.y, playerPos.z);
+        playerPos = new Vector2(playerPos.x + translate, playerPos.y);
         transform.position = playerPos;
+    }
+
+    void Jump()
+    {
+        if (rigidBody.velocity.y < 0)
+        {
+            rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime;
+        }
+        else if (rigidBody.velocity.y > 0)
+        {
+            rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (2f - 1) * Time.deltaTime;
+        }
     }
 }
