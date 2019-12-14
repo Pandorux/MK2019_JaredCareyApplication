f5d5e547b2053ad1369e9559a4758b152e0b0188
Jared <jared.l.j.carey@gmail.com>
2019-12-14 22:33:52 +1000

    Created Unfinished KillPlayer Script

---
+++ MK2019_ApplicationGame_JaredCarey/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class KillPlayer : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            // TODO: Call Game End State
+        }
+    }
+}
